2a212f5deda97c5c213ade828506578d0e7a1310
arthurdossena <arthur0dossena@gmail.com>
2025-06-25 19:00:04 -0300

    Logout and initial User page

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { styles } from './styles';
+import { Ionicons } from '@expo/vector-icons';
+import { Text, View, Alert, TouchableOpacity } from 'react-native';
+import { useNavigation, NavigationProp } from '@react-navigation/native';
 
 export default function User() {
+  const navigation = useNavigation<NavigationProp<any>>();
+
+  const handleLogout = () => {
+    Alert.alert("Logout", "Are you sure you want to logout?", [
+      {
+        text: "Cancel",
+        style: "cancel"
+      },
+      {
+        text: "OK",
+        onPress: () => {
+          //console.log("User logged out");
+          return navigation.reset({routes:[{name: "Login"}]}); // Navigate to Login screen after logout
+        }
+      }
+    ]);
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>User</Text>
-    </View>
+    <View style={styles.container}>
+        <Text style={styles.name}>User</Text>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Ionicons name="exit" size={24} color="gray"/>
+        </TouchableOpacity>
+      </View>
   );
 }
--- src/pages/user/styles.tsx
+++ src/pages/user/styles.tsx
@@ -1,4 +1,25 @@
 import { StyleSheet, Dimensions } from "react-native";
 import { themes } from "../../global/themes";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: themes.colors.background,
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        paddingVertical: 40,
+    },
+    name: {
+        fontSize: 24,
+        color: "black",
+        fontWeight: "bold",
+        marginTop: 20,
+    },
+    logoutButton: {
+        position: "absolute",
+        bottom: 10,
+        right: 20,
+        padding: 10,
+    },
+});
